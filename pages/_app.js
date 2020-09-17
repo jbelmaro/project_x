939864735f81007a57df2ee6bb749d4ada34aa62
Jesús Belmar <jesus.belmar@bbva.com>
2020-09-17 18:44:41 +0200

    First Commit

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+// import '../styles/index.css'
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+import '../styles/index.css'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
